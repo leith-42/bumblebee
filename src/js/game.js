53449727787a450397b77dad5d77c64863f7965c
Matt Capitao <8594512+leith-42@users.noreply.github.com>
2026-03-15 12:51:07 -0400

    added music controls

--- src/js/game.js
+++ src/js/game.js
@@ -1,77 +1,98 @@
-import Level from "./level.js";
+﻿import Level from "./level.js";
 import Hive from "./hive.js";
 import Bee from "./bee.js";
 
 const Game = {
-  running: false,
-  paused: false,
-  keyMap: { 37: false, 38: false, 39: false, 40: false },
+    running: false,
+    paused: false,
+    keyMap: { 37: false, 38: false, 39: false, 40: false },
+    backgroundMusic: null,
 
-  init: () => {
-    Level.current = 1;
-    Game.lives = 5;
-    Game.score = 0;
-    Game.reset = false;
-    Game.lightningEffect = false;
-    Game.rainSound = null;
-    Game.fadeRainSound = false;
-    Game.thunderSound = null;
-    Game.fadeThunderSound = false;
-    Game.birdCount = 0;
-    $("#score").html(Game.score);
-    $("#lives").html(Game.startingLives);
-    Level.init();
-    Hive.init();
-    Bee.init();
-  },
+    init: () => {
+        Level.current = 1;
+        Game.lives = 5;
+        Game.score = 0;
+        Game.reset = false;
+        Game.lightningEffect = false;
+        Game.rainSound = null;
+        Game.fadeRainSound = false;
+        Game.thunderSound = null;
+        Game.fadeThunderSound = false;
+        Game.birdCount = 0;
+        $("#score").html(Game.score);
+        $("#lives").html(Game.startingLives);
+        Level.init();
+        Hive.init();
+        Bee.init();
 
-  start: () => {
-    Game.running = true;
-    Game.paused = false;
-    if (Bee.dead) {
-      Bee.init();
-    }
-  },
+        Game.backgroundMusic = new Audio("../audio/flight_of_the_bumblebee_2.mp3");
+        Game.backgroundMusic.loop = true;
+    },
 
-  pause: () => {
-    Game.paused = true;
-    Game.stop();
-  },
+    start: () => {
+        Game.running = true;
+        Game.paused = false;
+        Game.toggleBackgroundMusic()
+        if (Bee.dead) {
+            Bee.init();
+        }
+    },
 
-  stop: () => {
-    Game.running = false;
-  },
+    pause: () => {
+        Game.paused = true;
+        Game.stop();
 
-  end: () => {
-    Game.stop();
-    Game.clear();
-    Game.reset = true;
-  },
+    },
 
-  clear: () => {
-    $(".bird").each(function () {
-      $(this).remove();
-    });
+    stop: () => {
+        Game.running = false;
+        Game.toggleBackgroundMusic();
+    },
 
-    $(".flower").each(function () {
-      $(this).remove();
-    });
-  },
+    end: () => {
+        Game.stop();
+        Game.clear();
+        Game.reset = true;
+        Game.stopBackgroundMusic();
+    },
 
-  complete: () => {
-    Game.score += Game.lives * 100000;
-    $(".final_score").html(Game.score);
-    $("#mission_complete").show("slow");
-    $("#start").html("New Game");
-    Game.end();
-  },
+    clear: () => {
+        $(".bird").each(function () {
+            $(this).remove();
+        });
 
-  over: () => {
-    $(".final_score").html(Game.score);
-    $("#game_over").show("slow");
-    $("#start").html("New Game");
-    Game.end();
-  },
+        $(".flower").each(function () {
+            $(this).remove();
+        });
+    },
+
+    complete: () => {
+        Game.score += Game.lives * 100000;
+        $(".final_score").html(Game.score);
+        $("#mission_complete").show("slow");
+        $("#start").html("New Game");
+        Game.end();
+    },
+
+    over: () => {
+        $(".final_score").html(Game.score);
+        $("#game_over").show("slow");
+        $("#start").html("New Game");
+        Game.end();
+    },
+
+    toggleBackgroundMusic: () => {
+        if (Game.backgroundMusic.paused) {
+            Game.backgroundMusic.play();
+        } else {
+            Game.backgroundMusic.pause();
+        }
+    },
+
+    stopBackgroundMusic: () => {
+        Game.backgroundMusic.pause();
+        Game.backgroundMusic.currentTime = 0; // Reset to the beginning
+    },
 };
 
 export default Game;
